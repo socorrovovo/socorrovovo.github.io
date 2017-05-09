0421b3a6731a83acd6f3c91692a445000b473d72
Rafael França <rafaelfms@gmail.com>
2017-05-09 09:18:51 -0300

    Update app.config.js

--- scripts/app.config.js
+++ scripts/app.config.js
@@ -2,4 +2,9 @@ angular
   .module('socorroVovo')
   .constant('DB_CONFIG', {
     url: './data/data.json'
-  });
+  })
+  .config([
+    '$interpolateProvider', function($interpolateProvider) {
+      return $interpolateProvider.startSymbol('{(').endSymbol(')}');
+    }
+  ]);
