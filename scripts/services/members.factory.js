941f8f62e821dee8c2d8111a47bc510fc630a7be
Rafael França <rafaelfms@gmail.com>
2017-05-10 09:09:55 -0300

    Create members.factory.js

---
+++ scripts/services/members.factory.js
@@ -0,0 +1,35 @@
+(function(){
+    'use strict';
+
+    angular
+        .module('socorroVovo')
+        .factory('membersFactory', membersFactory)
+
+    membersFactory.$inject = ['DB_CONFIG', '$http', '$q'];
+
+    function membersFactory(DB_CONFIG, $http, $q) {
+        var self = this;
+        var deferred = $q.defer();
+        var service = {
+            getData: getData
+        };
+
+        return service;
+
+        function getData() {
+          $http.get(DB_CONFIG.members)
+            .then(
+              function (res) {
+                // console.log(res.data);
+                // return res.data;
+                deferred.resolve(res.data);
+              },
+              function (err) {
+                return err;
+              }
+            );
+          // console.log(deferred.promise);
+          return deferred.promise;
+        }
+    }
+})();
